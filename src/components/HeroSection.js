7999c4d20be84dd69830cbca0d5010bc8d73d8aa
Justin Gaddy <justingaddy1@gmail.com>
2021-02-12 16:44:23 -0500

    intial commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button'
+import './HeroSection.css';
+
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+           {/* <video src='/videos/video-2.mp4' autoPlay loop muted/> */}
+           <h1>Adventure Awaits!</h1>
+           <p>What are you wating for?</p>
+           <div className="hero-btns">
+               <Button className='btns' 
+               buttonStyle='btn--outline' 
+               buttonSize='btn--large'
+               >
+                   GET STARTED
+               </Button>
+               <Button className='btns' 
+               buttonStyle='btn--primary' 
+               buttonSize='btn--large'
+               >
+                   Watch Trailer <i className='far fa-play-cirlce'/>
+               </Button>
+           </div>
+        </div>
+    )
+}
+
+export default HeroSection;
